eb3c363b425547bc4bbab6e10a56fd2816ab93af
GoharHayyat <goharchisthi@gmail.com>
2024-01-20 03:51:16 +0500

    cc

--- routes/Ingredient.js
+++ routes/Ingredient.js
@@ -55,4 +55,30 @@ router.get("/ingredients", async (req, res) => {
   }
 });
 
+// Delete ingredient by ID
+router.delete("/ingredients/:id", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  try {
+    const { id } = req.params;
+    console.log(id);
+    // Check if the ID is valid
+    if (!id) {
+      return res.status(400).json({ error: "Invalid ID provided." });
+    }
+
+    // Find and delete the ingredient by ID
+    const deletedIngredient = await Ingredient.findByIdAndDelete(id);
+
+    if (!deletedIngredient) {
+      return res.status(404).json({ error: "Ingredient not found." });
+    }
+
+    res.status(200).json({ message: "Ingredient deleted successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 module.exports = router;
