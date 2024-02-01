e6622452ac6c8f482f7623037edd48eb31db573b
GoharHayyat <goharchisthi@gmail.com>
2024-02-01 18:17:09 +0500

    cc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoDBURL = "mongodb+srv://ali:pTxnhzNCFGAXTvkH@cluster0.5ss7bm8.mongodb.
 const ControlPanel = require("./db/ControlPanel");
 const MenuItem = require("./db/MenuItem");
 const Ingredient = require("./db/IngredientSchema");
-const authRouter  =require('./routes/auth')
+const authRouter = require('./routes/auth')
 const app = express();
 const cors = require("cors");
 var bodyParser = require("body-parser");
@@ -27,15 +27,39 @@ app.use("/Restock", express.static("Restock"));
 //   })
 // );
 
+// router.post('/manageFavorite', async(req, res) => {
+//     const { userId, favorites } = req.body;
+//     console.log(req.body)
+
+//     try {
+//         // Find the user by ID
+//         const user = await User.findById(userId);
+
+//         if (!user) {
+//             return res.status(404).json({ error: 'User not found' });
+//         }
+
+//         // Update the user's favorites
+//         user.favorites = favorites;
+//         await user.save();
+
+//         res.json({ success: true });
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).json({ error: 'Server error' });
+//     }
+// });
+
+
 app.use(cors());
 
 app.use(bodyParser.json());
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-    // parameterLimit: 1000000,
-    limit: "500mb",
-  })
+    bodyParser.urlencoded({
+        extended: true,
+        // parameterLimit: 1000000,
+        limit: "500mb",
+    })
 );
 
 // app.use(express.json());
@@ -45,72 +69,69 @@ app.use(
 
 const menuItemRouter = require("./routes/menuItemRoutes");
 const IngredientRouter = require("./routes/Ingredient");
-const RestockRouter= require('./routes/Restock')
+const RestockRouter = require('./routes/Restock')
 
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "MainMenu");
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
+    destination: (req, file, cb) => {
+        cb(null, "MainMenu");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
 });
 
 const upload = multer({ storage });
 
 app.post("/getproduct", upload.single("image"), (req, res) => {
-  const { title } = req.body;
+    const { title } = req.body;
 
-  const imagePath = `MainMenu/${req.file.filename}`;
-  fs.rename(req.file.path, imagePath, (err) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).send("Error saving file to server");
-    }
-    const newData = new cpModel({ title, image: imagePath });
-    newData
-      .save()
-      .then((data) => {
-        res.json(data);
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).send("Error saving data to database");
-      });
-  });
+    const imagePath = `MainMenu/${req.file.filename}`;
+    fs.rename(req.file.path, imagePath, (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send("Error saving file to server");
+        }
+        const newData = new cpModel({ title, image: imagePath });
+        newData
+            .save()
+            .then((data) => {
+                res.json(data);
+            })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).send("Error saving data to database");
+            });
+    });
 });
 
-app.post("/updateCategory/:id", upload.single("image"), async (req, res) => {
-  const { id } = req.params;
-  const { title } = req.body;
+app.post("/updateCategory/:id", upload.single("image"), async(req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
 
-  try {
-    let controlpanel = await ControlPanel.findById(id);
+    try {
+        let controlpanel = await ControlPanel.findById(id);
 
-    if (!controlpanel) {
-      return res.status(404).json({ message: "Control panel not found" });
+        if (!controlpanel) {
+            return res.status(404).json({ message: "Control panel not found" });
+        }
+        const prevTitle = controlpanel.title;
+        controlpanel.title = title;
+        console.log(prevTitle);
+        console.log(controlpanel.title);
+
+        // console?
+        if (req.file) {
+            controlpanel.image = `MainMenu/${req.file.filename}`;
+        }
+
+        const updatedControlpanel = await controlpanel.save();
+        res.status(200).json(updatedControlpanel);
+
+        const menuItemUpdateResult = await MenuItem.updateMany({ category: prevTitle }, { $set: { category: controlpanel.title } });
+        console.log(menuItemUpdateResult);
+    } catch (error) {
+        res.status(500).json({ message: "Something went wrong" });
     }
-    const prevTitle = controlpanel.title;
-    controlpanel.title = title;
-    console.log(prevTitle);
-    console.log(controlpanel.title);
-
-    // console?
-    if (req.file) {
-      controlpanel.image = `MainMenu/${req.file.filename}`;
-    }
-
-    const updatedControlpanel = await controlpanel.save();
-    res.status(200).json(updatedControlpanel);
-
-    const menuItemUpdateResult = await MenuItem.updateMany(
-      { category: prevTitle },
-      { $set: { category: controlpanel.title } }
-    );
-    console.log(menuItemUpdateResult);
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
-  }
 });
 
 // app.get("/getAllproduct", async (req, resp) => {
@@ -121,20 +142,20 @@ app.post("/updateCategory/:id", upload.single("image"), async (req, res) => {
 //   resp.send(data);
 //   //   resp.json(data);
 // });
-app.get("/getAllproduct", async (req, resp) => {
-  resp.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from all origins
-  const data = await ControlPanel.find();
-  resp.send(data);
+app.get("/getAllproduct", async(req, resp) => {
+    resp.setHeader("Access-Control-Allow-Origin", "*"); // Allow requests from all origins
+    const data = await ControlPanel.find();
+    resp.send(data);
 });
 
-app.delete("/deleteCategory/:id", async (req, resp) => {
-  const controlPanel = await ControlPanel.findById(req.params.id);
-  await MenuItem.deleteMany({ category: controlPanel.title });
+app.delete("/deleteCategory/:id", async(req, resp) => {
+    const controlPanel = await ControlPanel.findById(req.params.id);
+    await MenuItem.deleteMany({ category: controlPanel.title });
 
-  await ControlPanel.deleteOne({ _id: req.params.id });
+    await ControlPanel.deleteOne({ _id: req.params.id });
 
-  resp.json({ message: "Category deleted successfully" });
-  // resp.send(result);
+    resp.json({ message: "Category deleted successfully" });
+    // resp.send(result);
 });
 
 // app.post("/delitems/:id", (req, res) => {
@@ -159,15 +180,15 @@ app.delete("/deleteCategory/:id", async (req, resp) => {
 //     });
 // });
 
-app.get("/search/:key", async (req, resp) => {
-  let result = await ControlPanel.find({
-    $or: [{ title: { $regex: req.params.key } }],
-  });
-  resp.send(result);
+app.get("/search/:key", async(req, resp) => {
+    let result = await ControlPanel.find({
+        $or: [{ title: { $regex: req.params.key } }],
+    });
+    resp.send(result);
 });
 
 app.get("/", (req, resp) => {
-  resp.send("Server is working");
+    resp.send("Server is working");
 });
 
 app.use(menuItemRouter);
@@ -176,6 +197,6 @@ app.use(RestockRouter);
 app.use(authRouter)
 
 mongoose.connect(mongoDBURL).then((result) => {
-  console.log("connected");
-  app.listen(4500);
+    console.log("connected");
+    app.listen(4500);
 });
